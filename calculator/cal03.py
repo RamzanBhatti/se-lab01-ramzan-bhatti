6c67551489fb5ee4cd0ce4cfef795438eef67b61
Ramzan Bhatti <mrramzan730@gmail.com>
2022-09-27 16:58:15 +0500

    calculator

---
+++ calculator/cal03.py
@@ -0,0 +1,20 @@
+def add(n1,n2):
+    return n1+n2
+
+def sub(n1,n2):
+    return n1-n2
+
+def mul(n1,n2):
+    return n1*n2
+
+def div(n1,n2):
+    return n1//n2
+
+def square(n):
+    return n ** 2
+
+print(add(10,2))
+print(sub(10,2))
+print(mul(10,2))
+print(div(10,2))
+print(square(10))
